b1b2f4889105eb7bdf60690b6156cd91d55a81ab
roshanvino <roshanpreetham21@gmail.com>
2024-04-27 23:48:20 +0100

    PawPalUITests

---
+++ PawPalUITests/PawPalUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  PawPalUITestsLaunchTests.swift
+//  PawPalUITests
+//
+//  Created by Roshan Vino on 20/03/2024.
+//
+
+import XCTest
+
+final class PawPalUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
